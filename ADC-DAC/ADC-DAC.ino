a1995f127e9d3695aa33624f93caaf7c06ec4e2f
AlbertoB <alberto.battistel@rub.de>
2017-09-06 16:21:28 +0200

    I had a pc failure, but now I am back. I integrated PingPong.h into ADC-DAC.ino. Ready for the loops.

--- ADC-DAC/ADC-DAC.ino
+++ ADC-DAC/ADC-DAC.ino
@@ -3,11 +3,12 @@
 #include <array>
 #include <assert.h>
 #include "DAC.h"
+#include "PingPong.h"
 
 /*
  * DAC delay
  */
-uint16_t DelayDAC = 100; // in us
+//uint16_t DelayDAC = 100; // in us
 
 /* do you want to check timing?
  *  it print info about how many samples are collected per iteration
@@ -22,9 +23,9 @@ uint16_t DelayDAC = 100; // in us
  *  allow to stop the stream after collecting maxSamples
  */
 bool stop4Samples[2] = {false, false};    // flag, signal that it streamed all the data of channels 0
-uint32_t nSamples0 = 0;        // counter for data streamed for channel 0
-uint32_t nSamples1 = 0;        // counter for data streamed for channel 1
-uint32_t maxSamples = 9000;     // stop after
+uint32_t counterSamplesADC0 = 0;        // counter for data streamed for channel 0
+uint32_t counterSamplesADC1 = 0;        // counter for data streamed for channel 1
+// uint32_t maxSamples = 9000;     // stop after
 
 /* Settings for Ring buffer (DMA) and aquisition frequency */
 // DMA buffer size
@@ -34,7 +35,8 @@ uint32_t maxSamples = 9000;     // stop after
 uint16_t streamBuffer[2][BUFFERSIZE];
 
 // Frequency of acquistion (max 250 kHz with 8 bit resolution)
-#define FREQUENCY 3000   // Hz
+//#define FREQUENCY 3007   // Hz
+//Parameters.ADC_frequency
 
 /* Settings for ADC
  *  AVERAGE 32 OK at 10 kHz
@@ -43,8 +45,8 @@ uint16_t streamBuffer[2][BUFFERSIZE];
  *  RESOLUTION 12 at 10 kHz gives a floor noise better than 0.1 mV
  *  RESOLUTION 8 at 250 kHz gives a floor noise of 0.1 mV
  */
-#define AVERAGE 1    // max 8 at 50 kHz
-#define RESOLUTION 8 // bits
+//#define AVERAGE 1    // max 8 at 50 kHz
+//#define RESOLUTION 8 // bits
 
 /* flags and counter */
 // command to stop streaming
@@ -71,7 +73,7 @@ bool JustStart = true;
  */
  #define BEST_N_VAL 100
  #define TIME_1VAL 3e-6   // s
- int32_t Delay_loop = max((int32_t)(BEST_N_VAL*(1-TIME_1VAL*FREQUENCY)/FREQUENCY*1e6), 0);    // us
+ int32_t Delay_loop = max((int32_t)(BEST_N_VAL*(1-TIME_1VAL*Parameters.ADC_frequency)/Parameters.ADC_frequency*1e6), 0);    // us
 
 
 /*
@@ -106,7 +108,8 @@ void setup() {
      *
      */
     analogWriteResolution(12);
-    timerDAC.begin(DAC_callback, DelayDAC);
+    pinMode(pinDAC,OUTPUT);
+    timerDAC.begin(DAC_callback, Parameters.DAC_delay);
     //pinMode(pinDAC,OUTPUT);
     //analogWrite(pinDAC,0);
     //
@@ -115,9 +118,9 @@ void setup() {
     pinMode(adc_pin1, INPUT);
 
     //~ constexpr uint32_t pdb_trigger_frequency = FREQUENCY;
-    uint32_t pdb_trigger_frequency = FREQUENCY;
+    //uint32_t pdb_trigger_frequency = Parameters.ADC_frequency;
     
-    setADC(AVERAGE, RESOLUTION);
+    setADC(Parameters.ADC_average, Parameters.ADC_resolution);
     
     dma0.source((uint16_t&) ADC0_RA); // ADC result register
     //dma0.source(ADC0_RA); // ADC result register
@@ -143,10 +146,16 @@ void setup() {
     adc.adc1->setHardwareTrigger();
 
     // enable PDB
+    // this is part is completely obscure to me...
     SIM_SCGC6 |= SIM_SCGC6_PDB;
-    uint32_t mod = (F_BUS / pdb_trigger_frequency);   // PDB frequency
-    assert(mod <= 0x10000);
+    uint32_t mod = (F_BUS / Parameters.ADC_frequency);   // PDB frequency
+    Serial.println(F_BUS / Parameters.ADC_frequency);
+    Serial.println(mod);
+    //assert(mod <= 0x10000);
+    Serial.println(PDB0_MOD);
     PDB0_MOD = (uint16_t)(mod-1);
+    //PDB0_MOD = (uint32_t)(mod-1);
+    Serial.println(PDB0_MOD);
     PDB0_CH0C1 = PDB_CHnC1_TOS_1 | PDB_CHnC1_EN_1; // PDB triggers ADC0 SC1A
     PDB0_CH1C1 = PDB_CHnC1_TOS_1 | PDB_CHnC1_EN_1; // PDB triggers ADC1 SC1A
     PDB0_SC = ADC_PDB_CONFIG | PDB_SC_PRESCALER(0) | PDB_SC_MULT(0) | PDB_SC_LDOK;
@@ -172,25 +181,27 @@ void setup() {
     startConversion();
     
     delayMicroseconds(Delay_loop);
+    exit(0);
 }
+
 
 
 
 void loop() {
     // stop when it reaches the right number of samples
     #ifdef DEBUG
-		if (stop4Samples[0]) Serial.println("Done 0");
-		if (stop4Samples[1]) Serial.println("Done 1");
+    if (stop4Samples[0]) Serial.println("Done 0");
+    if (stop4Samples[1]) Serial.println("Done 1");
     #endif
     if (stop4Samples[0] && stop4Samples[1]) {
-		exit(0);
+    exit(0);
     }
 
     #ifdef ITERATIONS
         Serial.println(Iterations);
     #endif
-  	/* Read position on DMA buffer
-  	 */
+    /* Read position on DMA buffer
+     */
     noInterrupts();
     size_t buffer_idx0 = (((uint16_t*) dma0.destinationAddress()) - &buffer[0].v_adc0) / 2;
     size_t buffer_idx1 = (((uint16_t*) dma1.destinationAddress()) - &buffer[0].v_adc1) / 2;
@@ -209,11 +220,11 @@ void loop() {
      */
 
     if(streamData) { // do it only if
-		nvalues0 = StreamData(nSamples0, 0, &buffer[0].v_adc0, old_buffer_idx0, buffer_idx0);
-		nSamples0 = nSamples0 + (uint32_t)nvalues0;
+    nvalues0 = StreamData(counterSamplesADC0, 0, &buffer[0].v_adc0, old_buffer_idx0, buffer_idx0);
+    counterSamplesADC0 = counterSamplesADC0 + (uint32_t)nvalues0;
 
-		nvalues1 = StreamData(nSamples1, 1, &buffer[0].v_adc1, old_buffer_idx1, buffer_idx1);
-		nSamples1 = nSamples1 + (uint32_t)nvalues1;
+    nvalues1 = StreamData(counterSamplesADC1, 1, &buffer[0].v_adc1, old_buffer_idx1, buffer_idx1);
+    counterSamplesADC1 = counterSamplesADC1 + (uint32_t)nvalues1;
     }
 
     /* Print details for timing and streaming
@@ -228,40 +239,40 @@ void loop() {
      *  it is faster to send only 10-50 data values than more than 100 by a factor of 2-3
      */
     #ifdef DEBUG
-		Serial.println();
-		Serial.print("Number of values: ");
-		Serial.print(nvalues0);
-		Serial.print(", ");
-		Serial.println(nvalues1);
+    Serial.println();
+    Serial.print("Number of values: ");
+    Serial.print(nvalues0);
+    Serial.print(", ");
+    Serial.println(nvalues1);
 
-		time_us = micros()-time_us;
-		Serial.print("time: ");
-		Serial.print(time_us);
-		Serial.println(" us");
+    time_us = micros()-time_us;
+    Serial.print("time: ");
+    Serial.print(time_us);
+    Serial.println(" us");
 
-		Serial.print("delay time in the loop: ");
-		Serial.print(Delay_loop);
-		Serial.println(" us");
+    Serial.print("delay time in the loop: ");
+    Serial.print(Delay_loop);
+    Serial.println(" us");
 
-		Serial.print("Current Buffer index DMA is writing to: ");
-		Serial.print(buffer_idx0);
-		Serial.print(", ");
-		Serial.println(buffer_idx0);
+    Serial.print("Current Buffer index DMA is writing to: ");
+    Serial.print(buffer_idx0);
+    Serial.print(", ");
+    Serial.println(buffer_idx0);
 
-		Serial.print("Old Buffer index DMA was: ");
-		Serial.print(old_buffer_idx0);
-		Serial.print(", ");
-		Serial.println(old_buffer_idx1);
+    Serial.print("Old Buffer index DMA was: ");
+    Serial.print(old_buffer_idx0);
+    Serial.print(", ");
+    Serial.println(old_buffer_idx1);
 
-		float us4value = (float)time_us/(float)nvalues0;
-		Serial.print("us for value: ");
-		Serial.println(us4value);
+    float us4value = (float)time_us/(float)nvalues0;
+    Serial.print("us for value: ");
+    Serial.println(us4value);
 
-		Serial.print("number of samples: ");
-		Serial.print(nSamples0);
-		Serial.print(", ");
-		Serial.println(nSamples1);
-		Serial.println();
+    Serial.print("number of samples: ");
+    Serial.print(counterSamplesADC0);
+    Serial.print(", ");
+    Serial.println(counterSamplesADC1);
+    Serial.println();
     #endif
 
     // get last position in the ring buffer
@@ -276,7 +287,7 @@ void loop() {
 
     // no idea, it was here already
     if(adc.adc0->fail_flag || adc.adc1->fail_flag) {
-		Serial.printf("ADC error, ADC0: %x ADC1: %x\n", adc.adc0->fail_flag, adc.adc1->fail_flag);
+    Serial.printf("ADC error, ADC0: %x ADC1: %x\n", adc.adc0->fail_flag, adc.adc1->fail_flag);
     }
 
     /* Delay the next iteration
@@ -304,7 +315,7 @@ void startConversion()  {
  *  average, resoltution
  */
  
- void setADC(int8_t ADC_average, int8_t ADC_resolution)	{
+ void setADC(int8_t ADC_average, int8_t ADC_resolution) {
     adc.setAveraging(ADC_average);
     adc.setResolution(ADC_resolution);
 
@@ -349,25 +360,25 @@ void startConversion()  {
      *  ring buffer did not wrap
      *  no problem data are in the right consecutive order
      */
-    		if(old_buffer_ < buffer_) {
-    		// consecutive order
-      			for(size_t i = old_buffer_; i < buffer_; i++) {
-        				streamBuffer[bufferIndex][posStreamBuffer++] = *(DMAbufferAddress + i);
-        				nvalues++;
-      			}
-		}
+        if(old_buffer_ < buffer_) {
+        // consecutive order
+            for(size_t i = old_buffer_; i < buffer_; i++) {
+                streamBuffer[bufferIndex][posStreamBuffer++] = *(DMAbufferAddress + i);
+                nvalues++;
+            }
+    }
     /* CASE II
      *  ring buffer wrapped
      *  take data from old position until the end of the buffer and start over from the beginning
      */
         else {
             for(size_t i = old_buffer_; i < BUFFERSIZE; i++) {
-    			      streamBuffer[bufferIndex][posStreamBuffer++] = *(DMAbufferAddress + i);
-    			      nvalues++;
+                streamBuffer[bufferIndex][posStreamBuffer++] = *(DMAbufferAddress + i);
+                nvalues++;
             }
             for(size_t i = 0; i < buffer_; i++) {
-    			      streamBuffer[bufferIndex][posStreamBuffer++] = *(DMAbufferAddress + i);
-    			      nvalues++;
+                streamBuffer[bufferIndex][posStreamBuffer++] = *(DMAbufferAddress + i);
+                nvalues++;
             }
         }
     }
@@ -380,12 +391,12 @@ void startConversion()  {
     *    write bytes (1.3 us per data)
     */
     uint16_t last2Stream = posStreamBuffer;
-    if(NumberOfSamples+nvalues >= maxSamples)  {
-	// it is reaching the right number of samples to stream
-		uint32_t samplesTooMuch = NumberOfSamples + nvalues - maxSamples;
-		last2Stream = posStreamBuffer - (uint16_t)samplesTooMuch;
-		stop4Samples[bufferIndex] = true;
-		streamData = true;
+    if(NumberOfSamples+nvalues >= Parameters.ADC_nSamples)  {
+  // it is reaching the right number of samples to stream
+    uint32_t samplesTooMuch = NumberOfSamples + nvalues - Parameters.ADC_nSamples;
+    last2Stream = posStreamBuffer - (uint16_t)samplesTooMuch;
+    stop4Samples[bufferIndex] = true;
+    streamData = true;
     }
 
     // Advice how many data are going to be streamed
@@ -394,14 +405,15 @@ void startConversion()  {
 
     // Stream bytes
     for(uint16_t i = 0; i < last2Stream; i++) {
-	// stream
-		Serial.write(lowByte(streamBuffer[bufferIndex][i]));
-		Serial.write(highByte(streamBuffer[bufferIndex][i]));
-		/* use
-		* data = s.read(x) where x is the number of bytes (16 bits = 2 bytes)
-		* np.fromstring(data,dtype = np.uint16)
-		* to get the data in python
-		*/
+  // stream
+    Serial.write(lowByte(streamBuffer[bufferIndex][i]));
+    Serial.write(highByte(streamBuffer[bufferIndex][i]));
+    /* use
+    * data = s.read(x) where x is the number of bytes (16 bits = 2 bytes)
+    * np.fromstring(data,dtype = np.uint16)
+    * to get the data in python
+    */
     }
     return last2Stream;   // number of data streamed
 }
+
--- ADC-DAC/PingPong.h
+++ ADC-DAC/PingPong.h
@@ -17,11 +17,11 @@
 struct parameters4measurement {
   // default values
   uint16_t StartStop = 1;    // 0 ->start aquisition, running; 1-> finished to aquire, waiting for paramerters; 2-> stop teensy
-  float ADC_frequency = 1000;  // Hz
+  float ADC_frequency = 10000;  // Hz
   uint32_t ADC_nSamples = 1000; // number of samples to collect
-  uint16_t ADC_average = 32;         // average in the ADC
+  uint16_t ADC_average = 2;         // average in the ADC
   uint16_t ADC_resolution = 16;      // resolution of the ADC
-  uint32_t DAC_delay = 10;          // us delay of the DAC
+  uint32_t DAC_delay = 100;          // us delay of the DAC
   uint16_t DAC_type = 0;            // kind of signal to output (not implemented yet)
 
 } Parameters;
@@ -98,7 +98,7 @@ void obj4Parameters::update_value(char letter, float x) {
 			checkStartStop(x);      
 			break;
 		case 'F':   // ADC sampling frequency
-			Parameters.ADC_frequency = x;
+			Parameters.ADC_frequency = (uint32_t)x;
 			break;
 		case 'N':   // number of samples to collect and stream
 			Parameters.ADC_nSamples = (uint32_t)x;
